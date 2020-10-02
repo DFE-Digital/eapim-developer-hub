4e01345cf22cefd24746f37af423c1672b3fb925
ashley taylor <ashley@digicodeltd.com>
2020-10-02 17:31:45 +0100

    tncs

--- emails/support.js
+++ emails/support.js
@@ -1,25 +1,4 @@
 export const template = ({ fullname, email, api, reason, description }) => {
-  return `
-<div style="display:block;font-size:12px;margin-bottom:10px;">
-  <h4 style="display:block;margin-bottom:5px;font-weight:bold;font-size:14px;">Name</h4>
-  <p style="font-size:12px;>${fullname}</p>
-</div>
-<div style="display:block;font-size:12px;margin-bottom:10px;">
-  <h4 style="display:block;margin-bottom:5px;font-weight:bold;font-size:14px;">Email</h4>
-  <p style="font-size:12px;>${email}</p>
-</div>
-<div style="display:block;font-size:12px;margin-bottom:10px;">
-  <h4 style="display:block;margin-bottom:5px;font-weight:bold;font-size:14px;">Reason</h4>
-  <p style="font-size:12px;>${reason}</p>
-</div>
-${api !== undefined && `
-<div style="display:block;font-size:12px;margin-bottom:10px;">
-  <h4 style="display:block;margin-bottom:5px;font-weight:bold;font-size:14px;">API</h4>
-  <p style="font-size:12px;>${api}</p>
-</div>
-`}
-<div style="display:block;font-size:12px;margin-bottom:10px;">
-  <h4 style="display:block;margin-bottom:5px;font-weight:bold;font-size:14px;">Description</h4>
-  <p style="font-size:12px;>${description}</p>
-</div>`
+  return `Name: ${fullname}; Email: ${email}; Reason: ${reason}; API: ${api !== undefined && api}; Description: ${description};`
 }
+
---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react'
+import Content from '../content.json'
+import AccessChecker from 'components/common/AccessChecker'
+import ReturnTo from 'components/common/ReturnTo'
+import ContentBuilder from 'components/common/ContentBuilder'
+
+const page = 'TermsAndConditions'
+
+const TermsAndConditions = ({ msalConfig, router }) => {
+  return (
+    <Fragment>
+      <AccessChecker msalConfig={msalConfig} />
+      <ReturnTo parentPath={router.asPath} />
+      <div className='govuk-width-container'>
+        <div className='govuk-breadcrumbs'>
+          <ol className='govuk-breadcrumbs__list'>
+            <li className='govuk-breadcrumbs__list-item'>
+              <a className='govuk-breadcrumbs__link' href={Content['Home'].Url}>{Content['Home'].Page}</a>
+            </li>
+            <li className='govuk-breadcrumbs__list-item' aria-current='page'>{Content[page].Page}</li>
+          </ol>
+        </div>
+        <main className='govuk-main-wrapper' id='main-content' role='main'>
+          <div className='govuk-grid-row'>
+            <div className='govuk-grid-column-full'>
+              <h1 className='govuk-heading-xl'>{Content[page].Page}</h1>
+              <ContentBuilder sectionNav={false} data={Content[page].Content.Body} />
+            </div>
+          </div>
+        </main>
+      </div>
+    </Fragment>
+  )
+}
+
+export default TermsAndConditions
