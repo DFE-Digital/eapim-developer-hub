948591eceb8cd15b07c289e0cabf74e6a639de75
ashley taylor <ashley@digicodeltd.com>
2020-10-02 17:41:57 +0100

    email string

--- emails/support.js
+++ emails/support.js
@@ -1,4 +1,4 @@
 export const template = ({ fullname, email, api, reason, description }) => {
-  return `Name: ${fullname}; Email: ${email}; Reason: ${reason}; API: ${api !== undefined && api}; Description: ${description};`
+  return `Name: ${fullname}\nEmail: ${email}\nReason: ${reason}\nAPI: ${api === undefined ? '' : api}\nDescription: ${description}`
 }
 
