879c7f3dbc824420f15e69f2401c1df55c5e79b8
ashley taylor <ashley@digicodeltd.com>
2020-10-05 12:06:01 +0100

    verify

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require('./routes')
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handler = app.getRequestHandler()
+const handler = routes.getRequestHandler(app)
 
 app
   .prepare()
