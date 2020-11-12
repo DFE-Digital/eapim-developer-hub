9b88ce8375d5d7ec4592adbf1521796fc8a3d51e
unknown <ashley@digicodeltd.com>
2020-11-12 17:08:02 +0000

    closest polyfill

--- public/assets/js/gds-main.js
+++ public/assets/js/gds-main.js
@@ -1,3 +1,21 @@
+if (!window.Element.prototype.matches) {
+  window.Element.prototype.matches =
+    window.Element.prototype.msMatchesSelector ||
+    window.Element.prototype.webkitMatchesSelector
+}
+
+if (!window.Element.prototype.closest) {
+  window.Element.prototype.closest = function (s) {
+    var el = this
+
+    do {
+      if (window.Element.prototype.matches.call(el, s)) return el
+      el = el.parentElement || el.parentNode
+    } while (el !== null && el.nodeType === 1)
+    return null
+  }
+}
+
 const main = (callback) => {
   if (document.readyState !== 'loading') callback()
   else document.addEventListener('DOMContentLoaded', callback)
