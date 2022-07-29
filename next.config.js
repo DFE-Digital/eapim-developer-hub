8f7c31df08e3887320c4d79876cb12a355437afb
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-07-29 13:06:34 +0100

    Fix next config warnings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const { ASSET_HOST } = process.env
 
 // for those who using CDN
-const assetPrefix = ASSET_HOST || ''
+const assetPrefix = ASSET_HOST || '/'
 
 module.exports = {
   assetPrefix,
-  target: 'serverless',
   webpack: (config, { dev }) => {
     config.output.publicPath = `${assetPrefix}${config.output.publicPath}`
 
