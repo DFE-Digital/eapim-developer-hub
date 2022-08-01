d82c3709476a5d341b0ea75310437fd6f304c645
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-08-01 12:57:28 +0100

    Fix prod/next update issues

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const { ASSET_HOST } = process.env
 
 // for those who using CDN
-const assetPrefix = ASSET_HOST || '/'
+const assetPrefix = ASSET_HOST || ''
 
 module.exports = {
   assetPrefix,
-  target: 'serverless',
+  target: 'serverless', // this needs removing but breaks the build - is this because of static files?
   webpack: (config, { dev }) => {
     config.output.publicPath = `${assetPrefix}${config.output.publicPath}`
 
