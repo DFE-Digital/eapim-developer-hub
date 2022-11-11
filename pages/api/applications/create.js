2ba2a53e6396c5f00fd561527cd0f407c2a3f13c
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-11-11 18:27:21 +0000

    Handle token expiry error Add neccessary awaits to api/app/create

--- pages/api/applications/create.js
+++ pages/api/applications/create.js
@@ -1,8 +1,15 @@
 import { registerApplication } from '../../../lib/applicationService'
 import { verify } from 'checkAuth'
 
-export default function handler (req, res) {
-  var result = verify(req, res)
+export default async function handler (req, res) {
+  var result = null
+
+  try {
+    result = await verify(req, res)
+  } catch {
+    res.status(403).json({ message: 'Forbidden' })
+    return
+  }
 
   if (!result) {
     res.status(403).json({ message: 'Forbidden' })
@@ -10,7 +17,7 @@ export default function handler (req, res) {
   }
 
   if (req.method === 'POST') {
-    const registration = registerApplication(req.body)
+    const registration = await registerApplication(req.body)
     res.status(200).json(registration)
   } else {
     res.status(405).json({ message: 'Only POST requests accepted' })
