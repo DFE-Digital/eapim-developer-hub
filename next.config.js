2621dbeaaea2b97282cbab35f0effa2a6f38c938
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-11-16 11:35:58 +0000

    Remove unsued cdn settings from next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const { ASSET_HOST } = process.env
-
-// for those who using CDN
-const assetPrefix = ASSET_HOST || ''
-
 module.exports = {
-  assetPrefix,
   poweredByHeader: false,
   webpack: (config, { dev }) => {
-    config.output.publicPath = `${assetPrefix}${config.output.publicPath}`
-
     return config
   }
 }
