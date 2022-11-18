bd22eed194991c1f96db13e3bfff3ae5a593ecf1
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-11-18 10:36:34 +0000

    Convert api page to serversideprops

--- pages/apis/[slug].js
+++ pages/apis/[slug].js
@@ -4,7 +4,6 @@ import ErrorPage from 'components/pages/ErrorPage'
 import APISummary from 'components/APISummary'
 
 import { getApis, getApiTags, getSummary } from '../../lib/apiServices'
-import errorHandler from '../../lib/errorHandler'
 
 const ApiDetails = ({ api, summary, serverError }) => {
   if (serverError) return <ErrorPage {...serverError} />
@@ -16,22 +15,23 @@ const ApiDetails = ({ api, summary, serverError }) => {
   )
 }
 
-ApiDetails.getInitialProps = async ({ res, req, query }) => {
-  try {
-    const apis = await getApis()
-    if (!apis) return errorHandler(res)
+export async function getServerSideProps (context) {
+  const apis = await getApis()
+  if (!apis) throw new Error('Forbidden')
 
-    const api = apis.find(api => api.name === query.slug)
-    if (!api) return errorHandler(res)
+  const api = apis.find(api => api.name === context.query.slug)
+  if (!api) throw new Error('Forbidden')
 
-    api.tags = await getApiTags(api.name)
+  api.tags = await getApiTags(api.name)
 
-    const summary = await getSummary(api.tags.summary)
-    if (!summary) return errorHandler(res)
+  const summary = await getSummary(api.tags.summary)
+  if (!summary) throw new Error('Forbidden')
 
-    return { api, summary }
-  } catch (error) {
-    return errorHandler(res, error, 500)
+  return {
+    props: {
+      api,
+      summary
+    }
   }
 }
 
